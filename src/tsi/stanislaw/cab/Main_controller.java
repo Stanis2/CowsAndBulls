2a34ceab2c23da6dd62c8e0a66656dcc2d555e85
Stanis2 <stanis2@inbox.lv>
2018-10-23 21:01:16 +0300

    23.10.2018 - Tsi_upd1

--- src/tsi/stanislaw/cab/Main_controller.java
+++ src/tsi/stanislaw/cab/Main_controller.java
@@ -4,6 +4,7 @@ import javafx.scene.control.Spinner;
 import javafx.scene.control.TableView;
 
 public class Main_controller {
+    private int turnNr;
     public Spinner<Integer> num1;
     public Spinner<Integer> num2;
     public Spinner<Integer> num3;
@@ -16,6 +17,7 @@ public class Main_controller {
 
     public void send() {
         System.out.println("Pressed: Send");
+        turnNr++;
         int n1 = num1.getValue();
         int n2 = num2.getValue();
         int n3 = num3.getValue();
@@ -23,6 +25,7 @@ public class Main_controller {
         System.out.println("Numbers entered: " + n1 + n2 + n3 + n4);
         Turn turn = new Turn();
         turn.setGuess("" + n1 + n2 + n3 + n4);
+        turn.setTurnNr(turnNr);
 
         turns.getItems().add(turn);
     }
