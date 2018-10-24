c539fece904e85a91e87a04b3ff7f3dc86711486
Stjek <stanis2@inbox.lv>
2018-10-24 23:01:40 +0300

    Upd_1

--- src/tsi/stanislaw/cab/Main_controller.java
+++ src/tsi/stanislaw/cab/Main_controller.java
@@ -2,9 +2,13 @@ package tsi.stanislaw.cab;
 
 import javafx.scene.control.Spinner;
 import javafx.scene.control.TableView;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main_controller {
     private int turnNr;
+    private static Random rand = new Random();
+    private static Scanner scan = new Scanner(System.in);
     public Spinner<Integer> num1;
     public Spinner<Integer> num2;
     public Spinner<Integer> num3;
@@ -13,6 +17,8 @@ public class Main_controller {
 
     public void initialize() {
         System.out.println("Initialized");
+        String randNum = String.format("%04d%n", rand.nextInt(9999));
+        System.out.println(randNum);
     }
 
     public void send() {
@@ -28,5 +34,21 @@ public class Main_controller {
         turn.setTurnNr(turnNr);
 
         turns.getItems().add(turn);
+        String scanNum = scanNumber();
+        scanNum.equals(num1.getValue());
+    }
+
+    private String scanNumber() {
+        int scanNum1 = scan.nextInt(num1.getValue());
+        int scanNum2 = scan.nextInt(num2.getValue());
+        int scanNum3 = scan.nextInt(num3.getValue());
+        int scanNum4 = scan.nextInt(num4.getValue());
+        System.out.println("Scanned numbers: " + scanNum1 + scanNum2 + scanNum3 + scanNum4);
+
+        if (scanNum1 == num1.getValue() || scanNum2 == num2.getValue() || scanNum3 == num3.getValue() || scanNum4 == num4.getValue()) {
+            Turn n1 = new Turn();
+            n1.setGuess("1");
+        }
+        return null;
     }
 }
